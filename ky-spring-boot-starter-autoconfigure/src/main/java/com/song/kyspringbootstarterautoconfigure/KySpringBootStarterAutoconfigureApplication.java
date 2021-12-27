70e41af4eb7c4212f2633b9240b006f19c80c88a
Ni7 <syq278872032@gmail.com>
2021-12-27 20:52:44 +0800

    first commit

---
+++ ky-spring-boot-starter-autoconfigure/src/main/java/com/song/kyspringbootstarterautoconfigure/KySpringBootStarterAutoconfigureApplication.java
@@ -0,0 +1,13 @@
+package com.song.kyspringbootstarterautoconfigure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KySpringBootStarterAutoconfigureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KySpringBootStarterAutoconfigureApplication.class, args);
+    }
+
+}
